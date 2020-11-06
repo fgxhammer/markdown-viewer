42197904f3948c67467c1b6ba79001c22c4d3f59
Franz Gollhammer <franz.gollhammer@gmail.com>
2020-11-07 00:02:29 +0100

    Refactor to async await

--- app/index.js
+++ app/index.js
@@ -3,42 +3,22 @@ const promisify = require('util').promisify
 const readFile = promisify(require('fs').readFile)
 const { ipcMain } = require('electron')
 
-const readFileContent = (filePath) => {
-  readFile(filePath)
-    .then(content => {
-      fileContent = content.toString()
-      return fileContent
-    })
-    .catch(err => {
-      const error = new Error('ERROR REDING FILE')
-      error.message = err
-      console.error(error)
-    })
+const getFilesAsync = async (filePath) => {
+  const fileContent = (await readFile(filePath)).toString()
+  return fileContent
 }
 
-
-ipcMain.handle('get-file-from-user', (event) => {
-  let fileContent
-
-  dialog.showOpenDialog({
+ipcMain.handle('get-file-from-user', async event => {
+  const dialogResponse = await dialog.showOpenDialog({
     properties: ['openFile'],
     filters: [
       { name: 'Markdown Files', extensions: ['md'] },
       { name: 'Text Files', extensions: ['txt', 'text'] }
     ]
   })
-    .then(files => {
-      if (files.canceled) return
-      const filePath = files.filePaths[0]
-      readFileContent(filePath)
-        .then(ct => {
-          console.log(ct)
-        })
-      event.sender.send('selected-file', selectedFile)
-    })
-    .catch(err => {
-      console.error(err)
-    })
+  if (dialogResponse.canceled) return
+  const fileContent = await getFilesAsync(dialogResponse.filePaths[0])
+  event.sender.send('selected-file', fileContent)
 })
 
 app.on('ready', () => {
